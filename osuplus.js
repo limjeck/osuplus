a9c98292785f0b4601b9c9ef8da7a5b08b4d9407
limjeck <limjeck@yahoo.com.sg>
2016-01-13 18:12:01 +0800

    1.0

--- osuplus.js
+++ osuplus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         osuplus
 // @namespace    https://osu.ppy.sh/u/1843447
-// @version      0.1
+// @version      1.0
 // @description  show pp, selected mods and other stuff
 // @author       oneplusone
 // @include      http*://osu.ppy.sh/b/*
@@ -29,7 +29,11 @@ var apikey = null,
     scoresTableBodyRef = null,
     playerCountries = null,
     modBtns = [],
-    localUser = null;
+    localUser = null,
+    localScore = null,
+    friends = null,
+    scoreListing = null,
+    scoreListingTitlerow = null;
 
 var modnames = [
     {val: 1, name: "NoFail", short: "NF"},
@@ -47,19 +51,19 @@ var modnames = [
     {val: 4096, name: "SpunOut", short: "SO"},
     {val: 8192, name: "Relax2", short: "AP"},
     {val: 16384, name: "Perfect", short: "PF"},
-    {val: 32768, name: "Key4", short: "K4"},
-    {val: 65536, name: "Key5", short: "K5"},
-    {val: 131072, name: "Key6", short: "K6"},
-    {val: 262144, name: "Key7", short: "K7"},
-    {val: 524288, name: "Key8", short: "K8"},
+    {val: 32768, name: "Key4", short: "4K"},
+    {val: 65536, name: "Key5", short: "5K"},
+    {val: 131072, name: "Key6", short: "6K"},
+    {val: 262144, name: "Key7", short: "7K"},
+    {val: 524288, name: "Key8", short: "8K"},
     {val: 1048576, name: "FadeIn", short: "FI"},
     {val: 2097152, name: "Random", short: "RD"},
     {val: 4194304, name: "LastMod", short: "LM"},
-    {val: 16777216, name: "Key9", short: "K9"},
-    {val: 33554432, name: "Key10", short: "K10"},
-    {val: 67108864, name: "Key1", short: "K1"},
-    {val: 134217728, name: "Key3", short: "K3"},
-    {val: 268435456, name: "Key2", short: "K2"},
+    {val: 16777216, name: "Key9", short: "9K"},
+    {val: 33554432, name: "Key10", short: "10K"},
+    {val: 67108864, name: "Key1", short: "1K"},
+    {val: 134217728, name: "Key3", short: "3K"},
+    {val: 268435456, name: "Key2", short: "2K"},
 ],
     // if the first is set, the second has to be set also
     doublemods = [
@@ -83,30 +87,215 @@ function init(){
         hasKey = false;
         displayGetKey();
     }
-    songInfoRef = $("#songinfo")[0];
-    scoresTableRef = $(".beatmapListing")[0].children[0];
-    scoresTableBodyRef = scoresTableRef.children[0];
-    var songInfoLen = songInfoRef.children[0].children.length;
-    var osuLink = songInfoRef.children[0].children[songInfoLen-1].children[2].children[3].href.split("/");
+    songInfoRef = $("#songinfo");
+    scoresTableRef = $(".beatmapListing").children();
+    scoresTableBodyRef = scoresTableRef.children();
+    var osuLink = songInfoRef.children().children().last().children().eq(2).children().eq(3).attr("href").split("/");
     mapID = osuLink[osuLink.length - 1];
-    var mapsetIDLink = $(".beatmap_download_link")[0].href.split("/");
+    var mapsetIDLink = $(".beatmap_download_link").last().attr("href").split("/");
     mapsetID = mapsetIDLink[mapsetIDLink.length - 1];
     mapMode = getMapmode();
     playerCountries = GM_getValue("playerCountries", {});
     if(typeof localUserId !== "undefined"){
         localUser = localUserId;
     }
+    scoreListing = $(".beatmapListing");
+    scoreListingTitlerow = $(".titlerow");
     addBloodcatMirror();
     showMapValues();
+    friends = GM_getValue("friends", []);
+    getFriends(function(response){
+        friends = response;
+        GM_setValue("friends", friends);
+    });
     
     if(hasKey){
         putModButtons();
+        putRankingType();
         getScores({b:mapID, m:mapMode, limit:100}, function(response){
             result = response;
+            addScoreLeaderpp();
             addPPColHeader();
             updateScoresTable();
+            addSearchUser();
         });
     }
+}
+
+function addSearchUser(){
+    $(".content-with-bg").children("h2").before(
+        $("<div></div>").attr("id", "searchuser")
+        .append(
+            $("<strong>Search user:</strong>"),
+            $("<input>").attr({type: "text",
+                               id: "searchusertxt"})
+            .bind("enterKey", searchUserEnter)
+            .keyup(function(e){
+                if(e.keyCode == 13)
+                {
+                    $(this).trigger("enterKey");
+                }
+            }),
+            $("<div></div>").attr("id", "searchuserinfo").text("Searching...").hide(),
+            $("<div></div>").attr("class","beatmapListing")
+            .attr("id", "searchuserresult")
+            .append(
+                $("<table width=100% cellspacing=0></table>").append("<tbody></tbody>").append(
+                    scoreListingTitlerow.clone()
+                )/*
+                $("<table width=100% cellspacing=0>" +
+                  "<tbody><tr class=\"titlerow\"><th></th>" + 
+                  "<th><strong>Rank</strong></th>" +
+                  "<th><strong>Score</strong></th>" +
+                  "<th><strong>pp</strong></th>" +
+                  "<th><strong>Accuracy</strong></th>" +
+                  "<th><strong>Player</strong></th>" +
+                  "<th><strong>Max Combo</strong></th>" +
+                  "<th><strong>300 / 100 / 50</strong></th>" +
+                  "<th><strong>Geki</strong></th>" +
+                  "<th><strong>Katu</strong></th>" +
+                  "<th><strong>Misses</strong></th>" +
+                  "<th><strong>Mods</strong></th><th></th></tr></tbody></table>")*/
+            ).hide()
+        )
+    );
+}
+
+function searchUserEnter(){
+    $("#searchuserinfo").text("Searching").show();
+    $("#searchuserresult").hide();
+    var searchusername = $("#searchusertxt").val();
+    getScores({b:mapID, u:searchusername, m:mapMode, type:"string"}, function(response){
+        if(response && response.length > 0){
+            var searchResult = response[0];
+            $("#searchuserresult").find(".titlerow").nextAll().remove();
+            $("#searchuserresult").find(".titlerow").parent().children().last().after(makeScoreTableRow(searchResult, 0));
+            $("#searchuserinfo").hide();
+            $("#searchuserresult").show();
+        }else{
+            $("#searchuserinfo").text("No scores found :(");
+        }
+    });
+}
+
+function putRankingType(){
+    $(".content-with-bg").children("h2").after(
+        $("<div></div>").attr("id", "rankingtype").append(
+            $("<label></label>").append($("<input>").attr({type: "radio",
+                                                           name: "rankingtype",
+                                                           value: "global"})
+                                        .prop("checked", true)
+                                        .change(rankingTypeChanged),
+                                        "Global"),
+            $("<label></label>").append($("<input>").attr({type: "radio",
+                                                           name: "rankingtype",
+                                                           value: "friends"})
+                                        .change(rankingTypeChanged),
+                                        "Friends")
+        )
+    );
+}
+
+function rankingTypeChanged(){
+    var rankingType = $("input[name=rankingtype]:checked").val();
+    function enableModBtns(yesno){
+        for(var i=0; i<modBtns.length; i++){
+            modBtns[i].prop("disabled", !yesno);
+        }
+    }
+    if(rankingType == "global"){
+        enableModBtns(true);
+        if(timeoutID !== null) clearTimeout(timeoutID);
+        updateModScores();
+    }else if(rankingType == "friends"){
+        enableModBtns(false);
+        if(timeoutID !== null) clearTimeout(timeoutID);
+        updateFriendsScores();
+    }
+}
+
+function updateFriendsScores(){
+    var funs = [];
+    for(var i=0; i<friends.length+1; i++){
+        funs.push(function(uid){
+            return function(callback){
+                getScores({b:mapID, u:uid, m:mapMode, type:"id"}, function(response){
+                    if(response && response.length > 0){
+                        result.push(response[0]);
+                    }
+                    callback();
+                });
+            };
+        }(i<friends.length ? friends[i] : localUser));
+    }
+    result = [];
+    doManyFunc(funs, function(){
+        sortResult();
+        updateScoresTable();
+    });
+}
+
+function sortResult(){
+    result.sort(function(a,b){
+        return parseInt(b.score) - parseInt(a.score);
+    });
+}
+
+function doManyFunc(funs, finalcallback){
+    var done = 0, total = funs.length;
+    for(var i=0; i<total; i++){
+        funs[i](function(){
+            done++;
+            if(done == total){
+                finalcallback();
+            }
+        });
+    }
+    if(total === 0){
+        finalcallback();
+    }
+}
+
+function getFriends(callback){
+    GetPage("https://osu.ppy.sh/p/friends", function(response){
+        var friends = [];
+        response = response.replace(/<img[^>]*>/g,"");
+        $(response).find(".paddingboth").children("div").each(function(ind, ele){
+            friends.push($(ele).attr("user_id"));
+        });
+        callback(friends);
+    });
+}
+
+function isFriend(uid){
+    for(var i=0; i<friends.length; i++){
+        if(friends[i] == uid) return true;
+    }
+    return false;
+}
+
+function addScoreLeaderpp(){
+    var scoreLeaders = $(".scoreLeader");
+    if(scoreLeaders.length > 0) updateScoreLeaderpp(scoreLeaders.first(), result[0]);
+    if(scoreLeaders.length > 1){
+        getScores({b:mapID, u:localUser, m:mapMode, type:"id"}, function(response){
+            if(response.length > 0){
+                localScore = response[0];
+                updateScoreLeaderpp(scoreLeaders.eq(1), localScore);
+            }
+        });
+    }
+}
+
+function updateScoreLeaderpp(scoreLeader, score){
+    var rows = scoreLeader.children().children();
+    var numrows = rows.length;
+    rows.eq(1).children().last().attr("rowspan", numrows);
+    var rowclass = "row" + ((numrows+1)%2+1) + "p";
+    scoreLeader.children().append($("<tr></tr>").attr("class", rowclass)
+                                  .append($("<td><strong>pp</strong></td>"))
+                                  .append($("<td></td>").text(Math.round(score.pp)))
+                                 );
 }
 
 function displayGetKey(){
@@ -165,24 +354,18 @@ function showMapValues(){
 
 function addBloodcatMirror(){
     if(mapsetID !== null){
-        $(".beatmap_download_link").after("<br>",$("<a></a>").text("Bloodcat mirror").attr("href", "http://bloodcat.com/osu/s/" + mapsetID))
+        $(".beatmap_download_link").last().after("<br>",$("<a></a>").text("Bloodcat mirror").attr("href", "http://bloodcat.com/osu/s/" + mapsetID))
     }
 }
 
 function putModButtons(){
     var cbcounter = 0;
     function genCheckbox(value, lbltxt){
-        var id = "cb" + cbcounter;
-        cbcounter++;
-        var btn = $("<input>").attr({id:id, 
-                                     type:"checkbox",
+        var btn = $("<input>").attr({type:"checkbox",
                                      value:value})
                               .change(timeoutUpdate);
         modBtns.push(btn);
-        return [
-            btn,
-            $("<label></label>").attr("for", id).text(lbltxt)
-        ];
+        return $("<label></label>").append(btn, lbltxt);
     }
     $(".content-with-bg").find("h2").next().empty().append(
         genCheckbox("NM", "NoMod"),
@@ -204,20 +387,25 @@ function timeoutUpdate(){
     if(timeoutID != null){
         clearTimeout(timeoutID);
     }
-    timeoutID = setTimeout(function(modval){
+    timeoutID = setTimeout(function(){
         timeoutID = null;
-        if(modval < 0){
-            getScores({b:mapID, m:mapMode, limit:100}, function(response){
-                result = response;
-                updateScoresTable();
-            });
-        }else{
-            getScores({b:mapID, m:mapMode, limit:100, mods:modval}, function(response){
-                result = response;
-                updateScoresTable();
-            });
-        }
-    }, timeDelay, getSelectedMods());
+        updateModScores();
+    }, timeDelay);
+}
+
+function updateModScores(){
+    var modval = getSelectedMods();
+    if(modval < 0){
+        getScores({b:mapID, m:mapMode, limit:100}, function(response){
+            result = response;
+            updateScoresTable();
+        });
+    }else{
+        getScores({b:mapID, m:mapMode, limit:100, mods:modval}, function(response){
+            result = response;
+            updateScoresTable();
+        });
+    }
 }
 
 function getSelectedMods(){
@@ -260,67 +448,90 @@ function getMapmode(){
 }
 
 function addPPColHeader(){
-    $(".titlerow").children().eq(2).after("<th><strong>pp</strong></th>");
+    scoreListingTitlerow.children().eq(2).after("<th><strong>pp</strong></th>");
 }
 
 function addPPCol(){
     addPPColHeader();
-    $(".titlerow").nextAll().each(function(ind,ele){$(ele).children().eq(2).after("<td>" + result[ind].pp + "</td>");});
+    scoreListingTitlerow.nextAll().each(function(ind,ele){$(ele).children().eq(2).after("<td>" + result[ind].pp + "</td>");});
 }
 
 function updateScoresTable(){
-    $(".titlerow").nextAll().remove();
-    var maxscore
+    scoreListingTitlerow.nextAll().remove();
     for(var i=0; i<result.length; i++){
-        var acc = calcAcc(result[i]);
-        var rowclass;
-        if(localUser !== null && localUser.toString() === result[i].user_id){
-            rowclass = "row3p";
-        }else{
-            rowclass = "row" + (i%2 + 1) + "p";
-        }
-        $(".titlerow").parent().children().last().after(
-            $("<tr></tr>")
-            .attr("class", rowclass)
-            .attr("title", result[i].date)
-            .append($("<td></td>").text("#" + (i+1)),
-                    $("<td></td>").append(getRankImg(result[i].rank)),
-                    $("<td></td>").html(i==0 ? "<b>"+commarise(result[i].score)+"</b>" : commarise(result[i].score)),
-                    $("<td></td>").text(Math.round(result[i].pp)),
-                    $("<td></td>").html(acc==100 ? "<b>"+acc.toFixed(2) + "%</b>" : acc.toFixed(2) + "%"),
-                    $("<td></td>").append(function(playerid, img){
-                                              img.attr("class", "flag");
-                                              getPlayerCountry(playerid, function(country){
-                                                  img.attr("src", getCountryUrl(country));
-                                              });
-                                              return img;
-                                          }(result[i].user_id, $("<img></img>")),
-                                          "\n",
-                                          $("<a></a>")
-                                          .attr("href", "/u/"+result[i].user_id)
-                                          .text(result[i].username)),
-                    $("<td></td>").text(result[i].maxcombo),
-                    $("<td></td>").html(result[i].count300 + "&nbsp;&nbsp;/&nbsp;&nbsp;" + 
-                                        result[i].count100 + "&nbsp;&nbsp;/&nbsp;&nbsp;" +
-                                        result[i].count50),
-                    $("<td></td>").text(result[i].countgeki),
-                    $("<td></td>").text(result[i].countkatu),
-                    $("<td></td>").text(result[i].countmiss),
-                    $("<td></td>").text(getMods(result[i].enabled_mods)),
-                    $("<td></td>").append($("<a></a>")
-                                          .attr("onclick", "reportscore("+result[i].user_id+");")
-                                          .text("Report"))
-                   )
-        );
+        scoreListingTitlerow.parent().children().last().after(makeScoreTableRow(result[i], i+1));
     }
+}
+
+function makeScoreTableRow(score, rankno){
+    var acc = calcAcc(score);
+    var rowclass;
+    if(localUser !== null && localUser.toString() === score.user_id){
+        rowclass = "row3p";
+    }else if(isFriend(score.user_id)){
+        rowclass = "row4p";
+    }else{
+        rowclass = "row" + ((rankno+1)%2 + 1) + "p";
+    }
+    return $("<tr></tr>")
+        .attr("class", rowclass)
+        .attr("title", score.date)
+        .append(
+        $("<td></td>").text(rankno>0 ? "#" + rankno : ""),
+        $("<td></td>").append(getRankImg(score.rank)),
+        $("<td></td>").html(rankno===1 ? "<b>"+commarise(score.score)+"</b>" : commarise(score.score)),
+        $("<td></td>").text(Math.round(score.pp)),
+        $("<td></td>").html(acc==100 ? "<b>"+acc.toFixed(2) + "%</b>" : acc.toFixed(2) + "%"),
+        $("<td></td>").append(function(playerid, img){
+            img.attr("class", "flag");
+            getPlayerCountry(playerid, function(country){
+                img.attr("src", getCountryUrl(country));
+            });
+            return img;
+        }(score.user_id, $("<img></img>")),
+                              "\n",
+                              $("<a></a>")
+                              .attr("href", "/u/"+score.user_id)
+                              .text(score.username)),
+        $("<td></td>").text(score.maxcombo),
+        mapMode === 3 ? 
+        // Mania
+        [$("<td></td>").text(score.countgeki),
+         $("<td></td>").text(score.count300),
+         $("<td></td>").text(score.countkatu),
+         $("<td></td>").text(score.count100),
+         $("<td></td>").text(score.count50)] : 
+        // Standard/Taiko/CTB
+        [$("<td></td>").html(score.count300 + "&nbsp;&nbsp;/&nbsp;&nbsp;" + 
+                             score.count100 + "&nbsp;&nbsp;/&nbsp;&nbsp;" +
+                             score.count50),
+         $("<td></td>").text(score.countgeki),
+         $("<td></td>").text(score.countkatu)],
+        $("<td></td>").text(score.countmiss),
+        $("<td></td>").text(getMods(score.enabled_mods)),
+        $("<td></td>").append($("<a></a>")
+                              .attr("onclick", "reportscore("+score.score_id+");")
+                              .text("Report"))
+    );
 }
 
 function calcAcc(score){
     var c50   = parseInt(score.count50),
         c100  = parseInt(score.count100),
         c300  = parseInt(score.count300),
-        cmiss = parseInt(score.countmiss);
-    return 100.0 * (6*c300 + 2*c100 + c50) / (6*(c50+c100+c300+cmiss));
+        cmiss = parseInt(score.countmiss),
+        cgeki = parseInt(score.countgeki),
+        ckatu = parseInt(score.countkatu);
+    if(mapMode === 0 || mapMode === 1){
+        // Standard/Taiko
+        return 100.0 * (6*c300 + 2*c100 + c50) / (6*(c50 + c100 + c300 + cmiss));
+    }else if(mapMode === 2){
+        //CTB
+        return 100.0 * (c300 + c100 + c50) / (c300 + c100 + c50 + ckatu + cmiss);
+    }else{
+        //Mania
+        return 100.0 * (6*cgeki + 6*c300 + 4*ckatu + 2*c100 + c50) / (6*(c50 + c100 + c300 + cmiss + cgeki + ckatu));
+    }
 }
 
 function getMods(modnum){
@@ -383,6 +594,20 @@ function commarise(num){
 function getRankImg(rank){
     var newimg = $("<img></img>").attr("src", "/images/"+rank+"_small.png");
     return newimg;
+}
+
+function getReplay(params, callback){
+    /*
+    k - api key (required).
+    m - the mode the score was played in (required).
+    b - the beatmap ID (not beatmap set ID!) in which the replay was played (required).
+    u - the user that has played the beatmap (required).
+    */
+    var url = "https://osu.ppy.sh/api/get_replay";
+    params.k = apikey;
+    GetPage(getUrl(url, params), function(response){
+        callback(JSON.parse(response));
+    });
 }
 
 function getScores(params, callback){
